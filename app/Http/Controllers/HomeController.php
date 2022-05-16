8da7609f1de9d4dc2a73d4986106edde61e283f2
Visalpou <povvisal001@gmail.com>
2022-05-16 16:06:28 +0700

    db update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,6 @@ class HomeController extends Controller
         $data['aboutus']= DB::table('portfolio')->first();
         $data['service']= DB::table('service')->get();
         $data['news']= DB::table('news')->get();
-
         return view('client.pages.hero-area',$data);
     }
 }
